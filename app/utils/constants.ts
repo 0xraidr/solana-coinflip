990abd1bf143df3f031975ca7a222f42a8445838
raidr <decentralizedju@gmail.com>
2023-02-15 11:04:06 -0500

    update

--- app/utils/constants.ts
+++
@@ -1,7 +0,0 @@
-import { clusterApiUrl, PublicKey } from "@solana/web3.js";
-
-export const PROGRAM_ID = new PublicKey(
-  "t8sQWcvrATnFFMLo9Ex96G4e8bNAMW4hW79rx8bdPa7"
-);
-
-export const SOLANA_RPC = clusterApiUrl("devnet");
